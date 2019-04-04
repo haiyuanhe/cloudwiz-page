a3c4a0ec0fdb10322dbdbeff69fa5fc97c9008b1
Rain120 <chao1202016@outlook.com>
2019-04-04 17:56:11 +0800

    update sql

--- server/db/newsSql.js
+++ server/db/newsSql.js
@@ -1,10 +1,10 @@
 module.exports = {
   createNews: `CREATE TABLE news (
     id int(255) NOT NULL AUTO_INCREMENT,
-    type varchar(255) CHARACTER SET utf8 NOT NULL,
-    title varchar(255) CHARACTER SET utf8 NOT NULL,
-    description varchar(255) CHARACTER SET utf8 NOT NULL,
-    occur_time varchar(255) CHARACTER SET utf8 NOT NULL,
+    type varchar(255) NOT NULL,
+    title varchar(255) NOT NULL,
+    description text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    occur_time varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
     url varchar(255) NOT NULL,
     newsImg varchar(255) NOT NULL,
     PRIMARY KEY (id)
